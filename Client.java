198247965cf1fc96159de893f4580295aeeec24b
harshadac2410 <111185574+harshadac2410@users.noreply.github.com>
2024-04-26 00:40:16 +0530

    just updated

--- Client.java
+++ Client.java
@@ -1,26 +1,15 @@
-package com.arraydemo;
-
-import java.util.Scanner;
+package com.methodoverloadingdemo;
 
 public class Client {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		int result=MathEngine.add(12, 5);
+		String resultstring=MathEngine.add("Hello", " World");
 		
-		Scanner in=new Scanner(System.in);
-		int []arr;  //arr is reference for an array
-		arr=new int[5]; //memory for 5 int is allocated on heap
-		
-		for(int i=0;i<arr.length;i++)
-		{
-			System.out.println("Enetr the ele:");
-			arr[i]=in.nextInt();
-		}
-		
-		for(int i=0;i<arr.length;i++)
-		{
-			System.out.println("ele-"+arr[i]);
-		}
+		System.out.println("the addition of int "+result);
+		System.out.println("the addition of string "+resultstring);
 	}
 
 }
